dea6028786dd3c4662fda960f5f387157cac7147
Rogério Pontes <1406314+rogerioacp@users.noreply.github.com>
2024-02-17 13:14:00 +0100

    First Deployment

---
+++ src/navigation.js
@@ -0,0 +1,14 @@
+export const headerData = {};
+
+export const footerData = {
+  socialLinks: [
+    { ariaLabel: 'LinkedIn', icon: 'tabler:brand-github', href: 'https://github.com/rogerioacp' },
+    { ariaLabel: 'GitHub', icon: 'tabler:brand-linkedin', href: 'https://www.linkedin.com/in/rogerioacp' },
+    { ariaLabel: 'Substack', icon: 'tabler:mail', href: 'https://rogeriopontes.substack.com/' },
+    { ariaLabel: 'X', icon: 'tabler:brand-x', href: 'https://twitter.com/rogerio_acp' },
+  ],
+  footNote: `
+    <span class="w-5 h-5 md:w-6 md:h-6 md:-mt-0.5 bg-cover mr-1.5 rtl:mr-0 rtl:ml-1.5 float-left rtl:float-right rounded-sm bg-[url(https://onwidget.com/favicon/favicon-32x32.png)]"></span>
+    Made by AstroWind · All rights reserved.
+  `,
+};
